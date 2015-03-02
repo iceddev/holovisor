b3e2634ddccba6cd2efec5438a5eb8c9664b6985
Blaine Bublitz <blaine@iceddev.com>
2015-03-02 12:21:41 -0700

    add travis, stub tests

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # holovisor
+
+[![Travis Build Status](https://img.shields.io/travis/iceddev/holovisor/master.svg?label=travis&style=flat-square)](https://travis-ci.org/iceddev/holovisor)
+
 Layout manager plugin for Irken
---
+++ test/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var lab = exports.lab = require('lab').script();
+var code = require('code');
+
+lab.experiment('holovisor', function(){
+
+  lab.test('renders layout', function(done){
+    // TODO: DOM testing?
+    done();
+  });
+});
